5ea32b1b72a90d600f109c333b24c12909d8504f
Jorge Aparicio <jorge.aparicio.r@gmail.com>
2012-09-07 10:22:22 -0500

    Added copyright notice.

--- loggerwidget.cpp
+++ loggerwidget.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2012 Jorge Aparicio <jorge.aparicio.r@gmail.com>
+ *
+ * This file is part of qSerialTerm.
+ *
+ * qSerialTerm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * qSerialTerm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with qSerialTerm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "loggerwidget.h"
 #include "ui_loggerwidget.h"
 
